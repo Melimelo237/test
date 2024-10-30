b8db4cad4b2673bbdf813203d7080da7c0770f87
Melimelo237 <lemagicienmerlin@gmail.com>
2024-10-30 15:03:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('interestForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // empêcher la soumission par défaut du formulaire
+
+    const interestLevel = document.getElementById('interest').value;
+
+    if (interestLevel === 'beaucoup') {
+        alert('Tu es très importante pour moi ! ❤️');
+    } else if (interestLevel === 'un peu') {
+        alert('Tu me plais bien ! 😊');
+    } else {
+        alert('Je suis désolé, je ne comprends pas ton niveau d\'intérêt.');
+    }
+});
